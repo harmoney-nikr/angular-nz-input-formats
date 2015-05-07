08bacc7ce945603216dca749004e8ba91f1a3d97
Nik Rolls <nik@rolls.cc>
2015-05-07 22:15:34 +1200

    WIP for strict phone validation

--- dist/angular-nz-input-formats.js
+++ dist/angular-nz-input-formats.js
@@ -505,26 +505,51 @@ var NZInputFormats;
                 return output;
             }
             var raw = NZPhoneNumber.sanitise(output);
+            if (angular.isDefined(this.options['intl'])) {
+                if (this.options['intl']) {
+                    raw = raw.match(/^(6|64.*)?/)[0];
+                }
+                else {
+                    raw = raw.match(/^(0.*)?/)[0];
+                }
+            }
             var intl = raw.substr(0, 2) === '64';
             if (intl) {
                 raw = '0' + raw.substr(2);
             }
+            var type;
             if (raw.match(/^0[89]0/)) {
+                type = 'special';
                 this.setMask(intl ? this.intlSpecialMask : this.specialMask);
                 this.minLength = intl ? 11 : 10;
             }
             else if (raw.substr(0, 2) === '02') {
+                type = 'mobile';
                 this.setMask(intl ? this.intlMobileMask : this.mobileMask);
                 this.minLength = intl ? 10 : 9;
             }
             else if (raw.match(/^0[345679]/)) {
+                type = 'landline';
                 this.setMask(intl ? this.intlLandlineMask : this.landlineMask);
                 this.minLength = intl ? 10 : 9;
             }
             else {
+                type = 'other';
                 this.setMask(this.defaultMask);
                 this.minLength = 9;
             }
+            switch (this.options['type']) {
+                case 'special':
+                    raw = raw.match(/^(?:0[89]0.*|0[89]|0)?/)[0];
+                    break;
+                case 'mobile':
+                    raw = raw.match(/^(?:02[1257].*|02|0)?/)[0];
+                    break;
+                case 'landline':
+                    raw = raw.match(/^(?:0[345679].*|0)?/)[0];
+                    break;
+            }
+            output = intl ? raw.replace(/^0/, '64') : raw;
             return _super.prototype.formatter.call(this, output);
         };
         NZPhoneNumber.prototype.validator = function () {
